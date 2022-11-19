afd80ea869b4f0746df203e3f0ffe187cab67725
Jupiter-J <namju2912@gmail.com>
2022-11-19 16:03:38 +0900

    init: project setting

---
+++ EyesTalkDB/src/main/java/dev/kakao5/eyestalkdb/EyesTalkDbApplication.java
@@ -0,0 +1,13 @@
+package dev.kakao5.eyestalkdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyesTalkDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EyesTalkDbApplication.class, args);
+    }
+
+}
---
+++ EyesTalkDB/src/test/java/dev/kakao5/eyestalkdb/EyesTalkDbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kakao5.eyestalkdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyesTalkDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
