0d16b0faac1abbba308ee9cfc14c35491b104472
Jupiter-J <namju2912@gmail.com>
2022-11-22 14:05:46 +0900

    feat: project property setting

--- EyesTalkDB/src/main/java/dev/kakao5/eyestalkdb/config/SwaggerConfig.java
+++ EyesTalkDB/src/main/java/dev/kakao5/eyestalkdb/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package dev.kakao5.eyestalkdb.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
