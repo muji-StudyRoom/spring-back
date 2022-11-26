592ecd42ab823d1dcb4792e230f51346e8ae664e
heejae <rhj0830@gmail.com>
2022-11-26 13:54:59 +0900

    edit jar

--- EyesTalkDB/src/main/java/dev/kakao5/eyestalkdb/config/WebConfig.java
+++ EyesTalkDB/src/main/java/dev/kakao5/eyestalkdb/config/WebConfig.java
@@ -11,8 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("**")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE");
+            registry.addMapping("/**");
     }
 }
